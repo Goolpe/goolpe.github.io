c81ff21cad90698acd2cde90747ad859f933b380
goolpe <gooolpe@gmail.com>
2020-05-08 21:22:49 +0300

    update mobile design

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,17 +1,16 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+  "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+"assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "734ba6ca772eaec64ae8c7bf4e4ad39b",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/LICENSE": "5d177a6950c6f25a9b9e19e90fcdefcd",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2def70cdfb9efbc99017bc85e0c5e804",
 "/": "2def70cdfb9efbc99017bc85e0c5e804",
-"main.dart.js": "47a0a541767ae5a807e9dd1b61833380",
+"main.dart.js": "909978ede25038f3fd4d520a43971c44",
 "manifest.json": "14e919b7b1dde549ab334b7ee3ce8412"
 };
 
