4c07af8464cab3370c1b9b65f7d7fed526a3d8a2
goolpe <gooolpe@gmail.com>
2020-05-11 14:12:02 +0300

    update icons

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -10,12 +10,12 @@ const RESOURCES = {
 "assets/LICENSE": "4a3b954fa3b131e24aa5d846dcc89b7a",
 "assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "e7dec9c5e1bd830c084f2d2fb94fa1e7",
 "favicon.png": "b1355b6505d5a52bffa5a2453fff5d40",
-"icons/icon-256.png": "8fbca49c79ae60339d29448745fb2127",
-"icons/icon-512.png": "28c052fc8ebb745f58a2a0fe27190e01",
-"index.html": "4ed49512f8cc449d10f78d1a1b9e010f",
-"/": "4ed49512f8cc449d10f78d1a1b9e010f",
+"icons/ic-256.png": "8fbca49c79ae60339d29448745fb2127",
+"icons/ic-512.png": "28c052fc8ebb745f58a2a0fe27190e01",
+"index.html": "ef00e318b087c6ae9b5f8bef63a6524f",
+"/": "ef00e318b087c6ae9b5f8bef63a6524f",
 "main.dart.js": "f028a47eef5461dfd0f7ac58f95c85f8",
-"manifest.json": "af810c5c9f84b09993e8f518ccb00e8c"
+"manifest.json": "3dab48bc69634ec1de7872ff5f31dd53"
 };
 
 self.addEventListener('activate', function (event) {
